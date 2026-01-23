a21141fc3d0c9ac5e928ccdccc95749bafc6855a
NyAinaMickael <ermiteandrianarivelo@gmail.com>
2026-01-23 14:51:09 +0300

    fix: correction configuration cors

--- gestion-backend/src/main/java/com/app/gestion/config/CorsConfig.java
+++ gestion-backend/src/main/java/com/app/gestion/config/CorsConfig.java
@@ -1,10 +1,12 @@
 package com.app.gestion.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@Configuration
 public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
@@ -12,7 +14,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         
