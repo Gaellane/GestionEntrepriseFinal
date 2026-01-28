daaa88de015a099120fe0285751d1248829d2308
NyAinaMickael <ermiteandrianarivelo@gmail.com>
2026-01-28 16:06:26 +0300

    chore: implementation des verifications de fond

--- gestion-backend/src/main/java/com/app/gestion/repository/CaisseMouvementRepository.java
+++ gestion-backend/src/main/java/com/app/gestion/repository/CaisseMouvementRepository.java
@@ -1,9 +1,18 @@
 package com.app.gestion.repository;
 
 import com.app.gestion.model.CaisseMouvement;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CaisseMouvementRepository extends JpaRepository<CaisseMouvement, Integer> {
+    List<CaisseMouvement> findByTypeMouvementId(Integer id);
+
+    @Query(nativeQuery = true, value = "SELECT SUM(montant) FROM caisse_mouvements WHERE type_mouvement_id = :id")
+    Double findMontantTotalByMouvementId(@Param("id") Integer id);
 }
