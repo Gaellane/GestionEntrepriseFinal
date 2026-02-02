0096f4f4d1d23625aaf35715df85b9ee5b7e6f3f
NyAinaMickael <ermiteandrianarivelo@gmail.com>
2026-02-02 20:56:30 +0300

    chore:ajout du component pour interaction avec IA au MainLayout

--- gestion-frontend/src/components/layout/MainLayout.jsx
+++ gestion-frontend/src/components/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "./SideBar";
+import AIChatWidget from "../common/AIChatWidget";
 
 const MainLayout = () => {
     return (
@@ -8,6 +9,7 @@ const MainLayout = () => {
             <div className="min-h-screen max-h-full bg-gradient-to-br from-gray-50 to-gray-100 p-6 overflow-y-scroll flex-1">
                 <Outlet />
             </div>
+            <AIChatWidget />
         </div>
     )
 };
