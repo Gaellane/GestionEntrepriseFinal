d561bf448ea05d7deef0a38c1d765b3673aca789
NyAinaMickael <ermiteandrianarivelo@gmail.com>
2026-02-02 20:53:52 +0300

    style: modification du format des requetes pour une compatibilite avec tous

--- gestion-backend/src/main/java/com/app/gestion/repository/LotRepository.java
+++ gestion-backend/src/main/java/com/app/gestion/repository/LotRepository.java
@@ -67,40 +67,38 @@ public interface LotRepository extends JpaRepository<Lot, Integer> {
     //@Query("SELECT l FROM Lot l WHERE l.article.id = :articleId AND l.depot.id = :depotId ORDER BY l.datePeremption ASC where l.quantite > 0")
     //public List<Lot> findFIFO(Integer articleId, Integer depotId,Double quantite);
 
-    @Query(value = """
-        SELECT *
-        FROM (
-            SELECT l.*,
-                SUM(l.quantite) OVER (ORDER BY l.date_arrivee ASC) AS cumul
-            FROM lots l
-            WHERE l.article_id = :articleId
-            AND l.depot_id = :depotId
-            AND l.quantite > 0
-        ) t
-            WHERE t.cumul <= :quantite
-            OR t.cumul - t.quantite < :quantite
-            ORDER BY t.date_arrivee ASC
-    """, nativeQuery = true)
+    @Query(value = "SELECT * " +
+                "FROM ( " +
+                "    SELECT l.*, " +
+                "        SUM(l.quantite) OVER (ORDER BY l.date_arrivee ASC) AS cumul " +
+                "    FROM lots l " +
+                "    WHERE l.article_id = :articleId " +
+                "    AND l.depot_id = :depotId " +
+                "    AND l.quantite > 0 " +
+                ") t " +
+                "WHERE t.cumul <= :quantite " +
+                "OR t.cumul - t.quantite < :quantite " +
+                "ORDER BY t.date_arrivee ASC", 
+        nativeQuery = true)
     List<Lot> findFIFO(
         @Param("articleId") Integer articleId,
         @Param("depotId") Integer depotId,
         @Param("quantite") Double quantite
     );
 
-    @Query(value = """
-        SELECT *
-        FROM (
-            SELECT l.*,
-                SUM(l.quantite) OVER (ORDER BY l.date_arrivee ASC) AS cumul
-            FROM lots l
-            WHERE l.article_id = :articleId
-            AND l.depot_id = :depotId
-            AND l.quantite > 0
-        ) t
-        WHERE t.cumul <= :quantite
-        OR t.cumul - t.quantite < :quantite
-            ORDER BY t.date_arrivee DESC
-    """, nativeQuery = true)
+    @Query(value = "SELECT * " +
+                "FROM ( " +
+                "    SELECT l.*, " +
+                "        SUM(l.quantite) OVER (ORDER BY l.date_arrivee ASC) AS cumul " +
+                "    FROM lots l " +
+                "    WHERE l.article_id = :articleId " +
+                "    AND l.depot_id = :depotId " +
+                "    AND l.quantite > 0 " +
+                ") t " +
+                "WHERE t.cumul <= :quantite " +
+                "OR t.cumul - t.quantite < :quantite " +
+                "ORDER BY t.date_arrivee DESC", 
+        nativeQuery = true)
     List<Lot> findLIFO(
         @Param("articleId") Integer articleId,
         @Param("depotId") Integer depotId,
@@ -108,19 +106,17 @@ public interface LotRepository extends JpaRepository<Lot, Integer> {
     );
 
 
-    @Query(value = """
-        SELECT *
-        FROM (
-            SELECT l.*,
-                SUM(l.quantite) OVER () AS cumul
-            FROM lots l
-            WHERE l.article_id = :articleId
-            AND l.depot_id = :depotId
-            AND l.quantite > 0
-        ) t
-        WHERE t.cumul <= :quantite
-        OR t.cumul - t.quantite < :quantite
-    """, nativeQuery = true)
+    @Query(value = "SELECT * "+
+        "FROM (" +
+            "SELECT l.*, " +
+                "SUM(l.quantite) OVER () AS cumul "+
+            "FROM lots l "+
+            "WHERE l.article_id = :articleId "+
+            "AND l.depot_id = :depotId "+
+            "AND l.quantite > 0 "+
+        ") t "+
+        "WHERE t.cumul <= :quantite"+
+        "OR t.cumul - t.quantite < :quantite", nativeQuery = true)
     List<Lot> findCMUP(
         @Param("articleId") Integer articleId,
         @Param("depotId") Integer depotId,
